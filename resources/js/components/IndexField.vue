f942dcb1ed88c9bbb49548179227a857b3bb48a0
mrajab <moh.rajab@dce.sa>
2018-09-01 15:29:37 +0300

    Display supported locales in the index view

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,7 +1,10 @@
 <template>
     <span>
-        <div v-for="locale in this.field.value.locales">
-            <a :href="getLangUrl(locale)">{{locale.label}}</a>
+        <div>
+            <a v-for="locale in this.field.value.locales"
+               :title=" (locale.translated?'Translated':'Untranslated')+' Language'"
+               :class="'btn btn-sm btn-default '+  (locale.translated?'btn-primary':'btn-secondary') "
+               :href="getLangUrl(locale)">{{locale.label}}</a>
         </div>
     </span>
 </template>
@@ -12,6 +15,7 @@
 
         methods: {
             getLangUrl(locale) {
+                console.log(this.field);
                 return Nova.config.base
                     + "/resources/"
                     + this.resourceName + "/"
@@ -21,3 +25,19 @@
         }
     }
 </script>
+
+
+<style>
+    .btn-sm {
+        margin-left: 10px;
+        padding-left: 5px;
+        padding-right: 5px;
+        font-size: 12px;
+        font-weight: normal;
+    }
+
+    .btn-secondary {
+        background-color: #c4c5d6;
+        color: black;
+    }
+</style>
