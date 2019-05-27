ae78827330bc7cf8b8f602882b1affff07cd8452
slovenianGooner <lovro.papez@gmail.com>
2019-05-27 08:29:02 +0200

    Change route type to delete

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
  Route::get('/current-local', 'Digitalcloud\MultilingualNova\Http\Controllers\LanguageController@currentLocal');
- Route::get('/remove-local/{locale}', 'Digitalcloud\MultilingualNova\Http\Controllers\LanguageController@removeLocal');
+ Route::delete('/remove-local/{locale}', 'Digitalcloud\MultilingualNova\Http\Controllers\LanguageController@removeLocal');
